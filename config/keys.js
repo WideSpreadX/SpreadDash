5a01a81215b150d9fd645494d9654042af5a6a5b
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2020-03-20 18:04:51 -0400

    changed config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   MongoURI:
     /*  "mongodb+srv://jonnyo:rootroot1@zer0ne-odl1z.mongodb.net/widespread?retryWrites=true&w=majority" */
-    "mongodb://jonnyo:rootroot1@ds011890.mlab.com:11890/heroku_g6zqmc8m"
+    "mongodb://heroku_g6zqmc8m:clmg1jvn0mqpcnph5ejpov422c@ds011890.mlab.com:11890/heroku_g6zqmc8m"
 };

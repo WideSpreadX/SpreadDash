f4c9a319cc0b23d5ea2d6a4eb8a7bd8dc3772f3d
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2020-03-20 18:19:57 -0400

    changed landing route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { ensureAuthenticated } = require("../config/auth");
 
 // Welcome Page
-router.get("/", (req, res) => res.render("welcome"));
+router.get("http://myspread.space/", (req, res) => res.render("welcome"));
 
 // Dashboard Page
 router.get("/dashboard", ensureAuthenticated, (req, res) =>

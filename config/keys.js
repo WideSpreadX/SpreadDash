16a58184190cd07af8c85497448f68b1ab2f2005
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2020-03-20 18:02:58 -0400

    add mLab uri for heroku

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://jonnyo:rootroot1@zer0ne-odl1z.mongodb.net/widespread?retryWrites=true&w=majority"
+    /*  "mongodb+srv://jonnyo:rootroot1@zer0ne-odl1z.mongodb.net/widespread?retryWrites=true&w=majority" */
+    "mongodb://jonnyo:rootroot1@ds011890.mlab.com:11890/heroku_g6zqmc8m"
 };
